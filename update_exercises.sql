1d50cebab8b6bfdd91275bc130bb7a92841b2ab2
Julian Lundquist <Retal@Julians-MacBook-Pro.local>
2018-11-30 16:58:50 -0600

    small update on basic statements

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,8 +1,10 @@
 use codeup_test_db;
 
-update albums set certified_sales=certified_sales * 10;
+-- update albums set certified_sales=certified_sales * 10;
+update albums set certified_sales = certified_sales * 10;
 select * from albums;
-update albums set release_date=1800 where release_date<=1980;
+-- update albums set release_date=1800 where release_date<=1980;
+update albums set release_date = release_date - 100 where release_date<=1980;
 select * from albums;
 update albums set artist='Peter Jackson' where artist='Michael Jackson';
 select * from albums;
